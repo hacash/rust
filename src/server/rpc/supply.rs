673f08501f8eee4d939e7d4b1213977c3de7b8f1
jojoin <2978963+jojoin@users.noreply.github.com>
2024-08-21 11:44:03 +0800

    up supply

--- src/server/rpc/supply.rs
+++ src/server/rpc/supply.rs
@@ -22,9 +22,21 @@ async fn supply(State(ctx): State<ApiCtx>, q: Query<Q9364>) -> impl IntoResponse
     
     // return data
     let mut data = jsondata!{
+        "latest_height", lasthei,
         "current_circulation", z2m(curr_ccl),
+
         "burned_fee",  z2m(burn_fee),
+        "burned_diamond_bid", z2m(*supply.hacd_bid_burn_zhu),
+        
+        "channel_deposit", z2m(*supply.channel_deposit_zhu),
         "channel_interest", z2m(*supply.channel_interest_zhu),
+        "channel_opening", *supply.opening_channel,
+        
+        "diamond_engraved", *supply.diamond_engraved,
+
+        "transferred_bitcoin", 0,
+        "trsbtc_subsidy", 0,
+
         "block_reward", z2m(blk_rwd),
         "minted_diamond", lastdia.number.uint(),
     };
