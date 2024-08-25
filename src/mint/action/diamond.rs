68dce07de2e2f848db95e5949a0a7b2e7c1f82f3
jojoin <2978963+jojoin@users.noreply.github.com>
2024-08-25 12:37:42 +0800

    up

--- src/mint/action/diamond.rs
+++ src/mint/action/diamond.rs
@@ -12,42 +12,13 @@
     (self, ctx, state, store, gas), // params
     false, // burn 90
     [], // req sign
-    diamond_transfer(self, ctx, state, store)
-}
-
-fn diamond_transfer(this: &DiamondSingleTransfer, ctx: &mut dyn ExecContext, sta: &mut dyn State, sto: &dyn Store) -> Ret<Vec<u8>> {
-    let from = ctx.main_address().clone();
-    let to = this.to.real(ctx.addr_list())?;
-    let mut diamonds = DiamondNameListMax200::default();
-    diamonds.push(this.diamond);
-    do_diamonds_transfer(&diamonds, from, to, ctx, sta, sto)
-}
-
-
-/**
- * Diamond Multiple From Transfer
- */
- ActionDefine!{
-    DiamondFromTransfer : 8, (
-        from           : AddrOrPtr 
-        diamonds       : DiamondNameListMax200 
-    ),
-    ACTLV_MAIN, // level
-    21, // gas
-    (self, ctx, state, store, gas), // params
-    false, // burn 90
-    [self.from], // req sign
     {
-        // gas = dia num * 6
-        gas += self.diamonds.count().uint() as i64 * DiamondName::width() as i64;
-        diamond_from_transfer(self, ctx, state, store)
+        let from = ctx.main_address().clone();
+        let to = self.to.real(ctx.addr_list())?;
+        let mut diamonds = DiamondNameListMax200::default();
+        diamonds.push(self.diamond);
+        do_diamonds_transfer(&diamonds, from, to, ctx, state, store)    
     }
-}
-
-fn diamond_from_transfer(this: &DiamondFromTransfer, ctx: &mut dyn ExecContext, sta: &mut dyn State, sto: &dyn Store) -> Ret<Vec<u8>> {
-    let from = this.from.real(ctx.addr_list())?;
-    let to = ctx.main_address().clone();
-    do_diamonds_transfer(&this.diamonds, from, to, ctx, sta, sto)
 }
 
 
@@ -68,14 +39,10 @@ fn diamond_from_transfer(this: &DiamondFromTransfer, ctx: &mut dyn ExecContext, 
     {
         // gas = dia num * 6
         gas += self.diamonds.count().uint() as i64 * DiamondName::width() as i64;
-        diamond_from_to_transfer(self, ctx, state, store)
+        let from = self.from.real(ctx.addr_list())?;
+        let to = self.to.real(ctx.addr_list())?;
+        do_diamonds_transfer(&self.diamonds, from, to, ctx, state, store)
     }
-}
-
-fn diamond_from_to_transfer(this: &DiamondFromToTransfer, ctx: &mut dyn ExecContext, sta: &mut dyn State, sto: &dyn Store) -> Ret<Vec<u8>> {
-    let from = this.from.real(ctx.addr_list())?;
-    let to = this.to.real(ctx.addr_list())?;
-    do_diamonds_transfer(&this.diamonds, from, to, ctx, sta, sto)
 }
 
 
@@ -97,14 +64,34 @@ fn diamond_from_to_transfer(this: &DiamondFromToTransfer, ctx: &mut dyn ExecCont
     {
         // gas = dia num * 6
         gas += self.diamonds.count().uint() as i64 * DiamondName::width() as i64;
-        diamond_to_transfer(self, ctx, state, store)
+        let from = ctx.main_address().clone();
+        let to = self.to.real(ctx.addr_list())?;
+        do_diamonds_transfer(&self.diamonds, from, to, ctx, state, store)
     }
 }
 
-fn diamond_to_transfer(this: &DiamondToTransfer, ctx: &mut dyn ExecContext, sta: &mut dyn State, sto: &dyn Store) -> Ret<Vec<u8>> {
-    let from = ctx.main_address().clone();
-    let to = this.to.real(ctx.addr_list())?;
-    do_diamonds_transfer(&this.diamonds, from, to, ctx, sta, sto)
+
+
+/**
+ * Diamond Multiple From Transfer
+ */
+ ActionDefine!{
+    DiamondFromTransfer : 8, (
+        from           : AddrOrPtr 
+        diamonds       : DiamondNameListMax200 
+    ),
+    ACTLV_MAIN, // level
+    21, // gas
+    (self, ctx, state, store, gas), // params
+    false, // burn 90
+    [self.from], // req sign
+    {
+        // gas = dia num * 6
+        gas += self.diamonds.count().uint() as i64 * DiamondName::width() as i64;
+        let from = self.from.real(ctx.addr_list())?;
+        let to = ctx.main_address().clone();
+        do_diamonds_transfer(&self.diamonds, from, to, ctx, state, store)
+    }
 }
 
 
